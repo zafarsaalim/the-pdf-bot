38586c975518ff1d163c29ac085a143c20d0c256
Saalim Zafar <saalimzafar46@gmail.com>
2026-09-06 05:16:07 +0530

    Add missing USDT_TRC_ADDRESS to config.py

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 
-import os
+#import os
 
 # Render will supply BOT_TOKEN from Environment Variables.
 # Locally, it will fall back to reading from a local .env file or environment variable.
+#BOT_TOKEN = os.environ.get("BOT_TOKEN")
+import os
+
+# Telegram Bot Token
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+# USDT Address imported by metaclean.py
+USDT_TRC_ADDRESS = os.environ.get("USDT_TRC_ADDRESS")
+
+USDT_TOKEN_ID = os.environ.get("USDT_TOKEN_ID")
