2fd4de7068487d906d725439989db22895ba6aca
Saalim Zafar <saalimzafar46@gmail.com>
2026-09-06 05:25:30 +0530

    Add missing USDT_TRC_ADDRESS to config.py

--- config.py
+++ config.py
@@ -14,3 +14,9 @@ USDT_TRC_ADDRESS = os.environ.get("USDT_TRC_ADDRESS")
 
 USDT_TOKEN_ID = os.environ.get("USDT_TOKEN_ID")
 USDT_PAYMENT_LINK = os.environ.get("USDT_PAYMENT_LINK")
+
+PRICING_TIERS = [
+    (5, 15),    # 1–5 pages = 10 USDT
+    (10, 25),   # 6–10 pages = 20 USDT
+    (float('inf'), 30)  # >10 pages = 30 USDT
+]
